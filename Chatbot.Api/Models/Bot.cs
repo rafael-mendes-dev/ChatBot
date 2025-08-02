41ad3623fe1358a279fcfc7edb76f460a7a5522c
Rafael Mendes <rafaelmen.ros@gmail.com>
2025-08-02 01:23:49 -0300

    fix: :sparkles: Adicionado limite de caracteres para o nome e contexto do bot

--- Chatbot.Api/Models/Bot.cs
+++ Chatbot.Api/Models/Bot.cs
@@ -9,10 +9,10 @@ public class Bot
 
     [MaxLength(50, ErrorMessage = "Nome do bot não pode ter mais de 50 caracteres."),
      MinLength(3, ErrorMessage = "Nome do bot deve ter pelo menos 3 caracteres.")]
-    public string Name { get; private set; } = string.Empty;
+    public string Name { get; set; } = string.Empty;
     [MaxLength(250, ErrorMessage = "Descrição do bot não pode ter mais de 250 caracteres."),
      MinLength(3, ErrorMessage = "Descrição do bot deve ter pelo menos 3 caracteres.")]
-    public string Context { get; private set; } = string.Empty;
+    public string Context { get; set; } = string.Empty;
     
     [JsonIgnore]
     public ICollection<Message> Messages { get; set; } = new List<Message>();
